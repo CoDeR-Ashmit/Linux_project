dd2a8c693884dcc6d5f1e127a3a7b7313ab94537
CoDeR-Ashmit <ashmitmudgal8@gmail.com>
2026-03-31 10:37:17 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Script 1: System Identity Report
-# Author: Ankshit | Course: Open Source Software
+# Author: Ashmit | Course: Open Source Software
 # --- Variables ---
-STUDENT_NAME="Ankshit"
+STUDENT_NAME="Ashmit"
 SOFTWARE_CHOICE="Git"
 
 # --- System info ---
